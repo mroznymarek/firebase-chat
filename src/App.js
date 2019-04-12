03a5a691b21ab91462baaa1c0b5207f6df34d480
Marek Mroz <marekmroz@wp.pl>
2019-04-12 09:37:12 +0200

    npm material-ui firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const App = (props) => (
   <div>
-      Chat
+      App
   </div>       
 )
 
---
+++ src/Auth.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Auth extends React.Component {
+    state = {
+        isUserLoggedIn: false,
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                this.state.isUserLoggedIn() ?
+
+                    :
+                    
+                }
+            </div>
+        )
+    }
+} 
+
+export default Auth
+
+
