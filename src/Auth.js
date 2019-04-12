b79c2fa06beda46d32067e7a498512fe9c181003
Marek Mroz <marekmroz@wp.pl>
2019-04-12 09:56:32 +0200

    button login and css styles

--- src/Auth.js
+++ src/Auth.js
@@ -1,24 +1,48 @@
+
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
+
+const styles ={
+    root: {
+        widht: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        
+    },
+    button: {}
+}
+
 class Auth extends React.Component {
-    state = {
-        isUserLoggedIn: false,
-    }
+  state = {
+    isUserLoggedIn: false,
+  }
 
-    render() {
-        return (
-            <div>
-                {
-                this.state.isUserLoggedIn() ?
+  render() {
+    return (
+      <div>
+        {
+          this.state.isUserLoggedIn ?
+            this.props.children
+            :
+            <div
+                style={styles.root}
+            >
+              <h1>JFDDL7 CHAT</h1>
+              <Button
+                variant={'contained'}
+                color={'secondary'}
+              >
+                LOGIN BY GOOGLE
+              </Button>
 
-                    :
-                    
-                }
             </div>
-        )
-    }
-} 
-
+        }
+      </div>
+    )
+  }
+}
 export default Auth
-
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import  './index.css'
+
 import App from './App'
+import Auth from './Auth'
 
 ReactDOM.render(
-    <App />, 
+    <Auth>
+        <App />
+    </Auth>,
     document.getElementById('root')
-    )
+)
 
-    
